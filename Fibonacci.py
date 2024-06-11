26da58d7e787442add8289d5ee31917d6bcd103e
TimofeyZimarin <timofey-zimarin2000@mail.ru>
2024-06-11 16:54:26 +0300

    Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,41 @@
+def fib(n):
+    if n == 1:
+        return 1
+    fib = []
+    fib.append(0)
+    fib.append(1)
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return (fib[-1])
+
+def fib_digit(n):
+    if n == 1:
+        return 1
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        temp = (fib[0] + fib[1]) % 10
+        fib[0], fib[1] = fib[1], temp
+    return fib[-1]
+
+def fib_mod(n, m):
+    if n == 1:
+        return m
+    fib = [0, 1]
+    ost = [0, 1]
+    for i in range(2, n + 1):
+        fib[0], fib[1] = fib[1], fib[0] + fib[1]
+        ost.append(fib[-1] % m)
+        if ost[-1] == 1 and ost [-2] == 0:
+            return ost[n % (len(ost) - 2)]
+    return fib[-1] % m
+
+
+def main():
+    n, m = map(int, input().split())
+    print(fib_mod(n, m))
+    #print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
+
