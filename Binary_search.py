ebc75f2edf63616ad9a637bc22bb73cd46c1ea1e
TimofeyZimarin <timofey-zimarin2000@mail.ru>
2024-07-20 11:36:43 +0300

    Algorithms

--- Binary_search.py
+++ Binary_search.py
@@ -1,9 +1,7 @@
-import math
 import sys
 from math import floor
 
 def Binary_search(a:list, i: int) -> int:
-    a.sort()
     l = 0
     r = len(a) - 1
     while l <= r:
@@ -20,10 +18,10 @@ def Binary_search(a:list, i: int) -> int:
 def main():
     reader = sys.stdin
     reader = [int(i) for i in next(reader).split()]
-    n, a = reader[0], reader[1:]
+    n, *a = reader
     reader = sys.stdin
     reader = [int(i) for i in next(reader).split()]
-    k, b = reader[0], reader[1:]
+    k, *b = reader
     for i in range(k):
         print(Binary_search(a, b[i]), end=' ')
 
