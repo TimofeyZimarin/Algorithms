f17e25d5504d1cc0b7b94227569519d189d83063
TimofeyZimarin <62215861+TimofeyZimarin@users.noreply.github.com>
2024-08-22 15:40:29 +0300

    Update Segments.py

--- Segments.py
+++ Segments.py
@@ -1,21 +1,63 @@
-def Segments():
-    n = int(input())
-    segments = []
-    for i in range(n):
-        a, b = map(int, input().split())
-        segments.append((a, b))
+import sys
 
-    segments.sort(key=lambda x: x[1])
 
-    i = 1
-    points = []
-    points.append(segments[0][1])
-    while i < len(segments):
-        if points[-1] >= segments[i][0]:
+def pockets(data, size):
+    stack = 0
+    time = 0
+    i = 0
+    j = 0
+    time_arr = []
+    if len(data) == 0:
+        return
+    while i < len(data):
+        if data[i][1] == 0:
+            time_arr.append(time)
             i += 1
+            j += 1
+            if stack != 0:
+                stack -= 1
+            if i + 1 <= len(data) and stack == 0:
+                time = data[i][0]
         else:
-            points.append(segments[i][1])
+            if i == 0:
+                time = data[i][0]
+            if data[i][1] != -1:
+                time_arr.append(time)
+                time += data[i][1]
+            else:
+                time_arr.append(-1)
+            while j < len(data) and time >= data[j][0]:
+                if time >= data[j][0] and stack < size:
+                    stack += 1
+                    j += 1
+                elif time > data[j][0] and stack == size:
+                    data[j][1] = -1
+                    j += 1
+                elif time == data[j][0] and stack == size:
+                    stack += 1
+                    j += 1
+                else:
+                    break
 
-    return print(len(points)), [print(i, end=' ') for i in points]
+            i += 1
+            stack -= 1
 
-Segments()
+
+
+
+    return [print(i) for i in time_arr]
+
+
+def main():
+    reader = sys.stdin
+    data = []
+    # size, n = map(int, next(reader).split())
+    # for i in range(n):
+    #     a, d = map(int, next(reader).split())
+    #     data.append([a, d])
+    data = [[999999, 1], [1000000, 0], [1000000, 1], [1000000, 0], [1000000, 0]]
+    size = 1
+    pockets(data, size)
+
+
+main()
